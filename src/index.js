19ea41042dbf4c38aa201334ae69e97bcab819a5
JoonHo Lee <joonhohere@gmail.com>
2020-03-03 14:24:41 -0800

    configuring brnaching mess

--- src/index.js
+++ src/index.js
@@ -24,6 +24,18 @@ let republicans = ['realDonaldTrump'];
 twitterScatterInstance.drawTwitterScatter(democrats, republicans, new Date('2019.10.01'),
     new Date('2019.12.25'), sentiments, "favorites");
 
+// You can load JSON files directly via require.
+// Note this does not add a network request, it adds
+// the data directly to your JavaScript bundle.
+// const exampleData = require('./example-data.json');
+
+
+// // Anything you put in the static folder will be available
+// // over the network, e.g.
+// d3.csv('carbon-emissions.csv')
+//   .then((data) => {
+//     console.log('Dynamically loaded CSV data', data);
+//   })
 
 /////////////////////
 // Filtering
