147908bc68be152d5e896b8257e57d0c6e8413c9
nancyli <nli57@cs.washington.edu>
2020-03-03 00:03:23 -0800

    Fix Date slider

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ $(function() {
         range: true,
         min: 0,
         max: 500,
-        values: [200, 300],
+        values: [0, 500],
         slide: function(event, ui) {
             $("#pop-amount-start").val(ui.values[0]);
             $("#pop-amount-end").val(ui.values[1]);
@@ -68,20 +68,49 @@ $(function() {
     $("#pop-amount-end").val($("#slider-range1").slider("values", 1));
 });
 
+// Initial view of Date slider
 $(function() {
     $("#slider-range2").slider({
         range: true,
-        min: new Date('2016.01.01').getTime() / 1000,
-        max: new Date('2020.01.01').getTime() / 1000,
-        values: [new Date('2017.01.01').getTime() / 1000, new Date('2019.01.01').getTime() / 1000],
-        slide: function(event, ui) {
-            $("#date-amount-start").val((new Date(ui.values[0] * 1000)).toLocaleDateString("en-US"));
-            $("#date-amount-end").val((new Date(ui.values[1] * 1000)).toLocaleDateString("en-US"));
-        }
+        min: new Date('2016.06.01').getTime() / 1000,
+        max: new Date('2017.03.01').getTime() / 1000,
+        values: [new Date('2016.06.01').getTime() / 1000, new Date('2017.03.01').getTime() / 1000],
+        disabled: true,
     });
-    $("#date-amount-start").val((new Date($("#slider-range2").slider("values", 0) * 1000)).toLocaleDateString("en-US"));
-    $("#date-amount-end").val((new Date($("#slider-range2").slider("values", 1) * 1000)).toLocaleDateString("en-US"));
 } );
+
+// Updated view of Date slider after user selects an election period
+$('input[type=radio]').change(function() {
+    if (this.value === "2016") {
+        $("#slider-range2").slider({
+            range: true,
+            min: new Date('2016.06.01').getTime() / 1000,
+            max: new Date('2017.03.01').getTime() / 1000,
+            values: [new Date('2016.06.01').getTime() / 1000, new Date('2017.03.01').getTime() / 1000],
+            disabled: false,
+            slide: function(event, ui) {
+                $("#date-amount-start").val((new Date(ui.values[0] * 1000)).toLocaleDateString("en-US"));
+                $("#date-amount-end").val((new Date(ui.values[1] * 1000)).toLocaleDateString("en-US"));
+            }
+        });
+        $("#date-amount-start").val((new Date($("#slider-range2").slider("values", 0) * 1000)).toLocaleDateString("en-US"));
+        $("#date-amount-end").val((new Date($("#slider-range2").slider("values", 1) * 1000)).toLocaleDateString("en-US"));
+    } else {
+        $("#slider-range2").slider({
+            range: true,
+            min: new Date('2019.06.01').getTime() / 1000,
+            max: new Date('2020.03.01').getTime() / 1000,
+            values: [new Date('2019.06.01').getTime() / 1000, new Date('2020.03.01').getTime() / 1000],
+            disabled: false,
+            slide: function(event, ui) {
+                $("#date-amount-start").val((new Date(ui.values[0] * 1000)).toLocaleDateString("en-US"));
+                $("#date-amount-end").val((new Date(ui.values[1] * 1000)).toLocaleDateString("en-US"));
+            }
+        });
+        $("#date-amount-start").val((new Date($("#slider-range2").slider("values", 0) * 1000)).toLocaleDateString("en-US"));
+        $("#date-amount-end").val((new Date($("#slider-range2").slider("values", 1) * 1000)).toLocaleDateString("en-US"));
+    }
+});
 /////////////////////
 d3.select("#form")
     .on("submit", function() {
